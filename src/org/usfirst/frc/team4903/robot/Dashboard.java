8869b38ac29e72226c2d4ec16dc7d62ff52145a9
Himel Mondal <himel131@gmail.com>
2015-03-05 16:27:16 -0500

    Basic Dashboard layout
    Have to still find out how to connect video stream to Mjpeg code,

--- src/org/usfirst/frc/team4903/robot/Dashboard.java
+++ src/org/usfirst/frc/team4903/robot/Dashboard.java
@@ -7,15 +7,27 @@ import javax.swing.*;
 public Class Dashboard{
 	JFrame dash = new JFrame("Dashboard");
 	JPanel p = new JPanel();
-
+	
+	//DashCamera cam1 = new DashCamera(x,y,w,h,name,ip);
+	//DashCamera cam2 = new DashCamera(x,y,w,h,name,ip);
+	
 	public static void main(String[] args) {
 		new Dashboard().run();
 	}
 	public void run(){
 		dash.setSize(1024, 710);
 		dash.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		p.setLayout(null);
 		p.setBackground(Color.white);
-		p.setBounds(0,0,,)
+		p.setSize(dash.getWidth(),dash.getHeight());
+		p.setBounds(0,0,dash.getWidth(),dash.getHeight());
+		
+		dash.add(p);
+		dash.setVisible(true);
+	}
+	public void display(){
+		//p.add(cam1);
+		//p.add(cam2);
 	}
 }
