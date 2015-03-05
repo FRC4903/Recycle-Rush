1c4592b3fed18df295c7065955914f27245cf5a3
Himel Mondal <himel131@gmail.com>
2015-03-05 10:53:20 -0500

    Basically recycled code from Team 3540
    Took out parts we didn't need or can't test atm

--- src/org/usfirst/frc/team4903/robot/DashCamera.java
+++ src/org/usfirst/frc/team4903/robot/DashCamera.java
@@ -4,6 +4,7 @@ import java.net.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+//Code credit to Wildcats Team 3540 for source code
 
 public Class DashCamera{
 	Jpanel p = new Jpanel();
@@ -22,35 +23,43 @@ public Class DashCamera{
 		this.ip = ip;
 	}
 	public void paintComponent(Graphics g){
-
+		Graphics2D canvas = (Graphics2D)g;
+		canvas.setColor(Color.white);
+		super.paintComponent(g);
+		this.setBackground(Color.black);
+		
+		if(jpg!=null){
+			img = jpg.getImage(); //sets frame as bufferedimage
+			canvas.drawImage(img,0,0,w,h,this); //paints frame on canvas
+		}
 	}
 	Class JpegInput extends Thread{
 		public void run(){
 			while (true){
 				try{
-					//jpgin = new MjpegInputStream(ip address camera input stream goes here)
-					if(cp.isVisible())
-					cp.setVisible(false);
+					HttpURLConnection ul= (HttpURLConnection) new URL(ip).openConnection();
+					jpgin = new MjpegInputStream(u1.getInputStream());
+					if(p.isVisible()) 			//this refreshes the content pane for each frame being displayed
+					p.setVisible(false);
 
 					while(!except){
-						try{jpg = jpgin.readMjpegFrame();}
+						try{jpg = jpgin.readMjpegFrame();}  //sets jpg as read Mjpeg frame (new frame from video output)
 						catch(IOException e){except = true;}
 						repaint();
 					}
 
 					jpgin.close();
-					p.setVisible(true);
+					p.setVisible(true); //displas frame
 				}
 				catch (Exception e){
-					if(!cp.isVisible())
-					cp.setVisible(true);
+					if(!p.isVisible())
+					p.setVisible(true);
 					repaint();
 				}
 			}
 		}
 	}
 	public BufferedImage getImage(){
-
+		return (BufferedImage)img;
 	}
-
 }
