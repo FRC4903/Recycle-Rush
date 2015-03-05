7baf2adc0260ed3d6a6bba43cf93604d26eca81c
Vasav Shah <vasav.shah97@gmail.com>
2015-03-05 16:03:16 -0500

    dash start

---
+++ src/org/usfirst/frc/team4903/robot/Dashboard.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.util.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+
+public Class Dashboard{
+	JFrame dash = new JFrame("Dashboard");
+	JPanel p = new JPanel();
+
+	public static void main(String[] args) {
+		new Dashboard().run();
+	}
+	public void run(){
+		dash.setSize(1024, 710);
+		dash.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p.setLayout(null);
+		p.setBackground(Color.white);
+		p.setBounds(0,0,,)
+	}
+}
