dfb63c577224bfa225bce0155a18ca3518a5a244
Roman Seviaryn <romansev@ymail.com>
2015-03-23 15:37:03 -0400

    Done mode 1 for autonomous
    Don't delete any of my code!!!!!!!

--- src/org/usfirst/frc/team4903/robot/Auto.java
+++ src/org/usfirst/frc/team4903/robot/Auto.java
@@ -8,14 +8,18 @@ package org.usfirst.frc.team4903.robot;
 public class Auto {
 
     Data library;
-    int arm_up, arm_down, arm_range;
+    Controls controls;
+    Sensors sensors;
+    int arm_up, arm_down, arm_range, tote_low, tote_high, tote_range;
 
     //                NO FUCKING MAGIC NUMBERS!!!!!
     int max_length=100;                     //THIS NEEDS TO CHANGE
     int length_robot =25;                   //THIS NEEDS TO CHANGE
 
     public Auto(Data d){
-        library =d;
+        library = d;
+        controls = library.getControls();
+        sensors = library.getSensor();
     }
 
     public void autonomous() {
@@ -26,52 +30,68 @@ public class Auto {
         */
         
         calibrateArm();
+        calibrateToteLifter();
 
         // Roman put your shit here
         if(mode == 1){
             // Roman's shit
 
             // gets the yellow tote right at the start
-            library.getControls().pickUpTote(50);       // need pickUpTote method that takes % of motor power as a parameter
-                                                        // -ve value makes tote arm go down, +ve value makes it go up
+            // tweak the numbers
+            for(int i = 0; i < 1000; i++){
+                controls.armUp()
+            }
+
+            for(int i = 0; i < 100000000; i++){
+                talonSet(1.0, 1.0, 1.0, 1.0);
+            }
 
         }
     }
 
+    public void moveArmToPos(int percent, int speed){
+        int val = arm_down + (int)(arm_range / 100.0 * percent);
+
+        if(val > sensors.getEncoderPositionC2()){
+            controls.armUp(speed);
+        }else if(val < sensors.getEncoderPositionC2()){
+            controls.armUp(-speed);
+        }
+    }
+
     public void calibrateArm(){
-        Controls control = library.getControls();
-        Sensors sensors = library.getSensor();
 
         while (sensors.getLimitCC()) {
-            control.armUp(25);                          // need armUp method that takes % of motor power as a parameter
-                                                        // -ve value makes arm go down, +ve value makes it go up
+            controls.armUp(0.25);
         }
-        arm_up = sensors.getEncoderPositionC2();        // need this method, gets encoder value on CTalon2
+        arm_up = sensors.getEncoderPositionC2();
 
         while (sensors.getLimitC()) {
-            control.armUp(-25);
+            controls.armUp(-0.25);
         } 
         arm_down = sensors.getEncoderPositionC2();
         arm_range = Math.abs(arm_up - arm_down);
 
         while (Math.abs(sensors.getEncoderPositionC2()) != Math.abs(arm_range/2)) {
-            control.armUp(20);
+            controls.armUp(0.2);
         }
-        control.armUp(0);
     }
 
-    public void retractArm(){
+    public void cRetractArm(){
+        // stands for container retract arm
+
         //This assumes you are going to be hooked to the container
         //Then it moves it up, and brings it and drops it in front and goes all the way down.
-        while (arm_range != Math.abs(library.getSensor().getEncoderPositionC1())) {
+        while (arm_range != Math.abs(sensors.getEncoderPositionC1())) {
                 //this just sets it to the middle, need to figure the exact value with expermientation
-               library.getControls().armUp(20);  
+               controls.armUp(20);  
         }
-        while ((max_length - length_robot) != library.getSensor().getEncoderPositionC2()) {
-            library.getControls().armOut(20);
+        while ((max_length - length_robot) != sensors.getEncoderPositionC2()) {
+            controls.armOut(20);
         }
-        while (library.getSensor().getLimitCC()) {
-            library.getControls().armUp(-20);
+        while (sensors.getLimitCC()) {
+            controls.armUp(-20);
         }
     }
+
 }
